5d4d26a79183888700c4404a93e039d982de41d4
Samiksha25 <samusakshi@gmail.com>
2021-07-25 19:22:00 +0530

    init message

---
+++ src/test/java/com/readText/nlp/coreNLPminiProject/CoreNlpMiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.readText.nlp.coreNLPminiProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreNlpMiniProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
